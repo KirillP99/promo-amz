e1cb12cd39222d9dff0efb9dc5d01c5fa9c4178f
David Potapkin <david.potapkin@dataart.com>
2020-10-11 22:02:30 +0300

    add listener for inputs

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const input = document.querySelector('.input');
+const prefixBlock = document.querySelector('.prefix-block');
+const btn = document.querySelector('.btn');
+
+btn.disabled = true;
+
+const changeInputEvent = new Event('input');
+
+input.addEventListener('input', () => {
+    if (input.value.indexOf('@') !== -1) {
+        btn.disabled = false;
+    } else if (input.value.indexOf('@') === -1) {
+        btn.disabled = true;
+    }
+});
+
+prefixBlock.addEventListener('click', e => {
+    e.preventDefault();
+
+    if (!input.value) {
+        return;
+    }
+
+    if (e.target.className === 'prefix' && input.value.indexOf('@') !== -1) {
+        input.value = input.value.replace(/(@.+)/g, e.target.innerText);
+        return;
+    }
+
+    if (e.target.className === 'prefix') {
+        input.value += e.target.innerText;
+        input.dispatchEvent(changeInputEvent);
+    }
+
+    input.removeEventListener('input', changeInputEvent);
+
+    return;
+});
