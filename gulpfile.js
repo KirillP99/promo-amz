cde308aea5bb4db99f492a0ab775669180b9109d
KPetkov <kpetkov@phasev.com>
2020-10-11 04:02:51 +0300

    add first layout

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ function watch() {
     }
   });
   gulp.watch('./sass/**/*.scss', style);
+  gulp.watch('./sass/**/*.scss').on('change', browserSync.reload);
   gulp.watch('./*.html').on('change', browserSync.reload);
   gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
