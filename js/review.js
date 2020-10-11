a154de29a5dd84de82afa3acb88e92be2c80ca0a
David Potapkin <david.potapkin@dataart.com>
2020-10-11 22:12:19 +0300

    add input check on review

---
+++ js/review.js
@@ -0,0 +1,12 @@
+const textarea = document.querySelector('.textarea');
+const btn = document.querySelector('.btn');
+
+btn.disabled = true;
+
+textarea.addEventListener('input', () => {
+    btn.disabled = false;
+
+    if (textarea.value.length < 2) {
+        btn.disabled = true;
+    }
+});
