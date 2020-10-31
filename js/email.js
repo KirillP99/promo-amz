b873ab4e036d5f9907356a02cd86fc63b49f2f41
KPetkov <kpetkov@phasev.com>
2020-10-31 17:59:28 +0300

    fix bugs

--- js/email.js
+++ js/email.js
@@ -1,10 +1,7 @@
 const input = document.querySelector('.input');
 const btn = document.querySelector('.btn');
-// const prefixBlock = document.querySelector('.prefix-block');
 
 btn.disabled = true;
-
-// const changeInputEvent = new Event('input');
 
 input.addEventListener('input', () => {
     if (input.value.indexOf('@') !== -1) {
@@ -13,28 +10,6 @@ input.addEventListener('input', () => {
         btn.disabled = true;
     }
 });
-
-// prefixBlock.addEventListener('click', e => {
-//     e.preventDefault();
-
-//     if (!input.value) {
-//         return;
-//     }
-
-    // if (e.target.className === 'prefix' && input.value.indexOf('@') !== -1) {
-    //     input.value = input.value.replace(/(@.+)/g, e.target.innerText);
-    //     return;
-    // }
-
-    // if (e.target.className === 'prefix') {
-    //     input.value += e.target.innerText;
-    //     input.dispatchEvent(changeInputEvent);
-    // }
-
-//     input.removeEventListener('input', changeInputEvent);
-
-//     return;
-// });
 
 btn.addEventListener('click', () => {
     location.href = 'review.html';
